116165540216e0aa727729b07d888aeb837f2910
Jordan Mateen <JordanMateen@jordans-air-4.attlocal.net>
2018-12-07 21:48:21 -0500

    Clean Up

--- main.js
+++ main.js
@@ -1,28 +1,37 @@
-var socket = io();
+var socket = io(); // creating io socket for use in app
+
+
+//using JQUERY
 
 $(() =>{
-    $('#send').click(()=>{
+    $('#send').click(()=>{  //eventlistner on click the message will post as well as retreive from server
+        
+        //grabbing data from user input field
         var  message = {
-            name : $('#name').val(), 
+            name : $('#name').val(),  
             messages : $('#message').val()
         }
-        postMessages(message);
+        postMessages(message); //posting messages 
     });
-    getMessages();
+    getMessages(); // get message
 });
 
-socket.on('message', addMessages)
+socket.on('message', addMessages) //adding messages without refershing page.. Test with to clients open
 
+
+//appending messages to page grabbig id of div
 function addMessages(message){
     $('#messages').append(`<h4> ${message.name} </h4> <p> ${message.messages} </p>`);
 }
 
+//looping through messages to add
 function getMessages(){
     $.get('http://localhost:3000/messages', (data) =>{
         data.forEach(addMessages);
     });
 }
 
+//post message to server
 function postMessages(message){
     $.post('http://localhost:3000/messages', message);
 }
--- server.js
+++ server.js
@@ -10,34 +10,28 @@ app.use(express.static(__dirname));//importing directory
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : false}));
 
-var messages = [
-    {
-        name: 'Jordan Mateen',
-        messages: 'Welcome Jordan'
-    },
-    {
-        name: 'Adam Mateen',
-        messages: 'Welcome Adam'   
-    }
-]
+var messages = [ /**Messages get pushed here */     ]
+
+//get method.
 app.get('/messages', function(req, res){
-    console.log(`Method : ${req.method}\nURL: '${req.url}'`);
-    res.send(messages);
+    console.log(`Method : ${req.method}\nURL: '${req.url}'`);// request information
+    //res.send(messages);
 
 });
 
+//posting messages 
 app.post('/messages', function(req, res){
-    console.log(`Method : ${req.method}\nURL: '${req.url}'`);
-    messages.push(req.body);
-    io.emit('message', req.body);
+    console.log(`Method : ${req.method}\nURL: '${req.url}'`); // request information
+    messages.push(req.body);  // pushing messages into the messages array.
+    io.emit('message', req.body); // emitting messgae body to screen with out page refresh
     res.sendStatus(200);
 
 });
 
-io.on('connection', (socket) =>{
+io.on('connection', (socket) =>{    //event listener for server io. When server is connected two way is open
     console.log('User Connected');
 });
 
-http.listen(3000);
+http.listen(3000); //listening on port 3000 --> localhost:3000
 
-console.log("Server Running on port 3000");
+console.log("Server Running on port 3000"); //prompt user server is running
